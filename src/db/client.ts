edd0a5a06edb67ba72dee3151e393eb738d277bb
Dracorfs <franciscosantopietro@hotmail.com>
2026-03-16 10:58:23 -0300

    fix vercel deploy module prisma/client error

--- src/db/client.ts
+++ src/db/client.ts
@@ -1,14 +1,14 @@
-import { PrismaClient } from '@prisma/client';
 import { PrismaNeon } from '@prisma/adapter-neon';
+import { PrismaClient } from '../generated/prisma/client';
 
 const globalForPrisma = globalThis as unknown as {
-  prisma: PrismaClient | undefined;
+  prisma: InstanceType<typeof PrismaClient> | undefined;
 };
 
 export const getPrismaClient = (databaseUrl: string) => {
   if (!globalForPrisma.prisma) {
     const adapter = new PrismaNeon({ connectionString: databaseUrl });
-    globalForPrisma.prisma = new PrismaClient({ adapter });
+    globalForPrisma.prisma = new PrismaClient({ adapter } as any);
   }
   return globalForPrisma.prisma;
 };
